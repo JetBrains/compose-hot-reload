7ed41b6cbbf4bc7778954360f1eb510b54c5fcce
Azat Abdullin <azat.abdullin@jetbrains.com>
2025-05-27 12:45:25 +0200

    Fix build error in bytecode analyzer

--- samples/bytecode-analyzer/src/jvmMain/kotlin/org/jetbrains/compsoe/reload/analyzer/app/Main.kt
+++ samples/bytecode-analyzer/src/jvmMain/kotlin/org/jetbrains/compsoe/reload/analyzer/app/Main.kt
@@ -24,7 +24,7 @@ import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
-import org.jetbrains.compose.reload.core.createLogger
+import org.jetbrains.compose.reload.logging.HotReloadLogger
 import org.jetbrains.compsoe.reload.analyzer.app.states.*
 import org.jetbrains.compsoe.reload.analyzer.app.ui.AppTheme
 import org.jetbrains.compsoe.reload.analyzer.app.ui.FileView
