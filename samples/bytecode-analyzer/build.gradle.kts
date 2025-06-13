911029c79eee8e52f3668a687af0ee0f5a6d3670
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2025-06-13 07:50:36 +0200

    v1.0.0-alpha11

--- samples/bytecode-analyzer/build.gradle.kts
+++ samples/bytecode-analyzer/build.gradle.kts
@@ -21,8 +21,8 @@ kotlin {
     }
 
     sourceSets.commonMain.dependencies {
-        implementation("org.jetbrains.compose.hot-reload:core:1.0.0-beta01-139")
-        implementation("org.jetbrains.compose.hot-reload:analysis:1.0.0-beta01-139")
+        implementation("org.jetbrains.compose.hot-reload:core:1.0.0-alpha11")
+        implementation("org.jetbrains.compose.hot-reload:analysis:1.0.0-alpha11")
 
         implementation("io.sellmair:evas:1.2.0")
         implementation("io.sellmair:evas-compose:1.2.0")
