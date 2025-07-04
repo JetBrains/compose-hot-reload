ea89ca8b262d8fa1a949267d241a3bdc5654dbf2
Nikita Lipsky <nikita.lipsky@jetbrains.com>
2025-07-04 19:45:20 +0300

    Resolve dirty resources based on ResourceContentHash annotation

--- hot-reload-analysis/src/main/kotlin/org/jetbrains/compose/reload/analysis/resolveDirty.kt
+++ hot-reload-analysis/src/main/kotlin/org/jetbrains/compose/reload/analysis/resolveDirty.kt
@@ -48,6 +48,7 @@ private fun Context.resolveDirtyRuntimeScopeInfos(
         resolveRemovedMethods(current, redefined)
 
     val dirtyFields = resolveDirtyFields(current, redefined) +
+        resolveDirtyComposeResourceAccessors(current, redefined) +
         resolveRemovedFields(current, redefined)
 
     val transitivelyDirty = resolveTransitivelyDirty(current, redefined, dirtyMethods, dirtyFields)
@@ -82,15 +83,31 @@ private fun resolveRemovedMethods(current: ApplicationInfo, redefined: Applicati
     }
 }
 
-private fun resolveDirtyFields(current: ApplicationInfo, redefined: ApplicationInfo): List<FieldInfo> {
+private fun resolveDirtyFields(
+    current: ApplicationInfo, redefined: ApplicationInfo,
+    predicate: (FieldInfo, FieldInfo) -> Boolean
+): List<FieldInfo> {
     return redefined.fieldIndex.mapNotNull { (fieldId, redefinedField) ->
         val previousField = current.fieldIndex[fieldId] ?: return@mapNotNull redefinedField
-        if (previousField.initialValue != redefinedField.initialValue) {
+        if (predicate(previousField, redefinedField)) {
             return@mapNotNull redefinedField
         }
         null
     }
 }
+
+private fun resolveDirtyFields(current: ApplicationInfo, redefined: ApplicationInfo): List<FieldInfo> =
+    resolveDirtyFields(current, redefined) { previousField, redefinedField ->
+        previousField.initialValue != redefinedField.initialValue
+    }
+
+private fun resolveDirtyComposeResourceAccessors(
+    current: ApplicationInfo,
+    redefined: ApplicationInfo
+): List<FieldInfo> =
+    resolveDirtyFields(current, redefined) { previousField, redefinedField ->
+        previousField.resourceContentHash != redefinedField.resourceContentHash
+    }
 
 private fun resolveRemovedFields(current: ApplicationInfo, redefined: ApplicationInfo): List<FieldInfo> {
     return redefined.classIndex.flatMap { (classId, redefinedClass) ->
@@ -293,7 +310,7 @@ private fun resolveTransitivelyDirty(
                 ?: current.classIndex[element.memberId.classId]
 
             classInfo?.fields?.forEach { (fieldId, field) ->
-                if (field.isStatic) {
+                if (field.isStatic && field.resourceContentHash == null) {
                     queue.add(Element(fieldId, depth = element.depth + 1))
                 }
             }
