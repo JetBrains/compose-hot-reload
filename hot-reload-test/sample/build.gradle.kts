0477bbb1d4c2f9848f0a433c96cfc62dde19a095
Azat Abdullin <azat.abdullin@jetbrains.com>
2025-09-11 15:57:58 +0200

    Target version 1.0.0-beta07

--- hot-reload-test/sample/build.gradle.kts
+++ hot-reload-test/sample/build.gradle.kts
@@ -18,7 +18,7 @@ kotlin {
     jvm()
 
     sourceSets.jvmMain.dependencies {
-        implementation("org.jetbrains.compose.hot-reload:runtime-api:1.0.0-rc01+167")
+        implementation("org.jetbrains.compose.hot-reload:runtime-api:1.0.0-beta07")
         implementation(compose.runtime)
     }
 
